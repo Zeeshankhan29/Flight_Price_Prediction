e3340165642ab016c98f72d0e5e352cfd73c2af2
Zeeshankhan29 <zeeshankhan29khan@gmail.com>
2022-11-19 11:24:51 +0530

    update

--- app.py
+++ app.py
@@ -339,7 +339,7 @@ def predict():
 
         output=round(prediction[0],2)
 
-        return render_template('home.html',prediction_text="Your Flight price is Rs. {}".format(output))
+        return render_template('home.html',prediction_text="Your Flight Price is Rs. {}".format(output))
 
 
     return render_template("home.html")
